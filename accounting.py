f4244f58be5aa331aaa16e8fd78a4d49b713df93
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-05-01 04:49:20 +0000

    Melon cost to outer scope, added $ and 0 to line 18, re-worked finding over/underpaid customers

--- accounting.py
+++ accounting.py
@@ -1,8 +1,8 @@
-
+melon_cost = 1.00
 #Create function with parameters"orders"
 def under_paid(orders):
   #establish melon cost
-  melon_cost = 1.00
+
   #parse line in order into elements
   for line in orders:
     line = line.rstrip()
@@ -15,13 +15,15 @@ def under_paid(orders):
     #find expected amount
     cost = num_melons * melon_cost
     #print what customers paid and expected amts
-    print(f"{name} paid ${paid}, expected {cost}.")
+    print(f"{name} paid ${paid}, expected ${cost}0.")
     #if-statement finding customers who overpaid
-    if cost < paid:
-      print(f"{name} is OVERPAID.")
-    #if-statement finding customers who underpaid
-    elif cost > paid:
-      print(f"{name} is UNDERPAID.") 
+    if cost != paid:
+      if cost < paid:
+        payment_status = ("OVERPAID")
+      #if-statement finding customers who underpaid
+      elif cost > paid:
+        payment_status = ("UNDERPAID") 
+      print(f"{name} has {payment_status}.")
   #close file
   orders.close()
 #Call function, pass customer-orders document as argument
